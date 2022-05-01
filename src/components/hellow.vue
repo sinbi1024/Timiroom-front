e78f5b5d58f3866bf9322b7819f3e6002e2640dd
Kim-Bomyong <81305023+qhaud98@users.noreply.github.com>
2022-05-01 23:28:51 +0900

    ref 사용법!

---
+++ src/components/hellow.vue
@@ -0,0 +1,42 @@
+<script lang="ts">
+import {defineComponent, ref, reactive} from "vue";
+export default defineComponent( {
+  name: "hellow",
+  setup(){
+    let count = ref<number>(0);
+    let name = ref<string>('신비')
+
+    const click = ()=>{
+      count.value += 1;
+      console.log(count.value); // 0
+    }
+
+    const getImage = (e:HTMLElement)=>{
+      console.log(e);
+      // if(e) const k = e.target;
+      // console.log(e?.target);
+    }
+
+    return{
+      count,
+      name,
+      click,
+      getImage
+    }
+},
+
+
+})
+</script>
+
+<template>
+<section>
+  <div>{{count}}</div>
+  <button @click="click">눌러죠</button>
+  <input type="file" @change="getImage">
+  <div>{{name}}</div>
+</section>
+</template>
+<style scoped>
+
+</style>
