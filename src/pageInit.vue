38e63ab73d570ddfc93219bf249ffa3e75413641
sinbi <snbiiiii1024@gmail.com>
2022-05-16 20:46:12 +0900

    Changes by bomyong

---
+++ src/pageInit.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+import {defineComponent, onMounted, ref} from "vue";
+
+export default defineComponent({
+  name: "PageInit",
+  setup() {
+
+    return {}
+  },
+
+});
+</script>
+
+<template>
+  <section>
+
+  </section>
+</template>
+
+<style scoped>
+</style>
