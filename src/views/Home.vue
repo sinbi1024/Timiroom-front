8ecdcea9f58c6249fe3ca565b312b648e833f0ce
sinbi <snbiiiii1024@gmail.com>
2022-05-22 16:14:12 +0900

    메인 페이지 1차 완성

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,39 @@
 <script lang="ts">
-import {defineComponent, ref, onMounted} from "vue";
+import {defineComponent, ref, onMounted, onUnmounted} from "vue";
 
 export default defineComponent({
   name: "PageInit",
   setup() {
+    const addScrollEvent = () => {
+      console.log('keyEventListener On!');
+      window.addEventListener('scroll', eventHandler);
+    }
+
+    const eventHandler = (e) => {
+      const scroll = window.scrollY;
+
+      if (scroll > 360 && scroll <= 497) {
+        console.log(scroll);
+      }
+
+      if (scroll > 1150 && scroll <= 1190) {
+        console.log(scroll);
+      }
+    }
+
+    const removeScrollEvent = () => {
+      console.log('remove evnet!');
+      window.removeEventListener('scroll', eventHandler)
+    }
+
+    onMounted(() => {
+      addScrollEvent()
+    });
+
+    onUnmounted(() => {
+      removeScrollEvent();
+    })
+
 
     return {}
   },
@@ -26,10 +56,23 @@ export default defineComponent({
         <div class="main-title">
           왜 스마트 팩토리입니까?
         </div>
+        <img class="point-why-smf">
         <div class="main-context">
           많은 기업이 사업 환경 변화에 따른 위기를 맞고 있습니다.<br>
           하지만 그동안 구축해온 생산 방식, 비용 중심의 제조 및 물류 체계는 다양성 수용 및 새로운 환경 적응 등 곳곳에서 한계를 갖습니다.<br>
           그래서 기업들은 <b>스마트 팩토리</b>를 선택하고 있습니다.
+        </div>
+      </div>
+
+      <div class="main-card">
+        <div class="main-title">
+          무엇이 필요합니까?
+        </div>
+        <img class="point-what-require">
+        <div class="main-context">
+          설비별로 발생하는 데이터를 빠르게 수집하여 <b>실시간 모니터링</b>을 제공할 수 있어야 합니다.<br>
+          제품 판매, 공급망 변화에 대해 생산 시점에서부터 <b>빠르고 유연하게 대응</b>할 수 있어야 합니다.<br>
+          <b>설비의 장애 예방 및 가동률 관리</b>를 위해 설비에서 발생하는 데이터를 활용하고자 합니다.
         </div>
       </div>
     </section>
